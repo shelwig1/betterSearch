449beb3d765cfffc836bd3c8e9040bc81c0fd1a0
Sean H <sean.helwig@gmail.com>
2025-01-25 18:44:21 -0500

    more fiddling with structure - need to do more backend before continuing

--- betterSearch/frontend/src/App.tsx
+++ betterSearch/frontend/src/App.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
+interface SearchResultProp {
+    target : string;
+    file : string;
+}
 const App = () => {
     return (
         <div>
@@ -8,6 +12,39 @@ const App = () => {
         </div>
     )
 }
+// Want to make it so as we spawn these cocksuckers, they get routed into here
+// Need to hold the function call for the search, then pass it on to the kids
+const SearchResultContainer = () => {
+    const [results, setResults] = useState<SearchResultProp[]>([])
 
+    const handleNewResult = (newResult: SearchResultProp) => {
+        setResults((prevResults) => [...prevResults, newResult])
+    }
+    // This is where we make our money - import the search function here, listen as we stream in more and more shit, ezpz
+/*      useEffect(() => {
+        const searchData = async () => {
+            const newResults = await fetchSearchResults()
+            handleNewResult(newResults)
+        }
+    }) */
+    return (
+        <div id='searchResultsContainer'>
+            {results.map((result, index) => (
+                <SearchResultCard key={index} target={result.target} file={result.file} />
+            ))}
+        </div>
+    )
+}
+const SearchResultCard: React.FC<SearchResultProp> = ({target, file}) => {
+    return (
+        <div className='searchResultCard'>
+            <p>{target}</p>
+            <p>{file}</p>
+        </div>
+    )
+}
 //ReactDOM.render(<App />, document.getElementById('app'))
+
+
+
 export default App;
